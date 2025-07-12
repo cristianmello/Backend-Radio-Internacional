26ada69225ba42a23f8e65b17e8f4e1919337054
cristianmello <mellocristian45@gmail.com>
2025-07-11 22:36:03 -0300

    fix(middleware): utilizar version node lts y cambio de config por railway

--- config/config.js
+++ config/config.js
@@ -11,11 +11,11 @@ module.exports = {
     logging: process.env.DB_LOGGING === 'true' ? console.log : false
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 3306,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     dialect: 'mysql',
     logging: false
   }

6978a29373ddc22c854484eb685ba45f02e5d855
cristianmello <mellocristian45@gmail.com>
2025-07-11 23:11:13 -0300

    chore: forzar redespliegue para refrescar cache

--- services/redisclient.js
+++ services/redisclient.js
@@ -47,6 +47,7 @@ module.exports = redisClient;
 
 // services/RedisClient.js
 const Redis = require('ioredis');
+// Forzando el redespliegue en Railway
 
 //const redisUrl = process.env.REDIS_URL || `redis://${process.env.REDIS_HOST || '127.0.0.1'}:${process.env.REDIS_PORT || 6379}`;
 const redisUrl = process.env.REDIS_URL
